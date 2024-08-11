a1f3b6094137979fe541bc614980d8634795aca3
Callum Thomson <callum@callumthomson.co.uk>
2024-08-11 12:30:47 +0100

    Add tags to the root stack

--- apps/cdk/bin/cdk.ts
+++ apps/cdk/bin/cdk.ts
@@ -2,9 +2,10 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
+import { Tags } from 'aws-cdk-lib';
 
 const app = new cdk.App();
-new CdkStack(app, 'DearNextVisitorStack', {
+const stack = new CdkStack(app, 'DearNextVisitorStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -22,3 +23,6 @@ new CdkStack(app, 'DearNextVisitorStack', {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+Tags.of(stack).add('Project', 'dear-next-visitor');
+Tags.of(stack).add('Environment', 'prod');
+Tags.of(stack).add('Repo', 'callumthomson/dear-next-visitor');
