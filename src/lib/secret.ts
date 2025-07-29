b26ec13d539ce1f880ce2c53fd8ccaeb38674614
Callum Thomson <callum@callumthomson.co.uk>
2025-07-29 21:35:59 +0100

    Update `docker-stack.yml` service image and refactor `secret.ts` utility for streamlined secret handling

--- src/lib/secret.ts
+++ src/lib/secret.ts
@@ -1,3 +1,18 @@
-  export const secret = async (secretName: string): Promise<string> => {
-    return (await Bun.file((process.env.SECRETS_DIR || '/run/secrets/') + secretName).text()).trim();
-  }
+const secretKeys = ['openai_api_key'] as const;
+type SecretKey = typeof secretKeys[number];
+
+// const secrets: Partial<Record<SecretKey, string>> = {};
+//
+// export const initSecrets = async () => {
+//   await Promise.all(secretKeys.map(async key => {
+//     try {
+//       secrets[key] = (await Bun.file((process.env.SECRETS_DIR || '/run/secrets/') + key).text()).trim();
+//     } catch (err) {
+//       console.warn(`Could not load '${key}' secret`)
+//     }
+//   }));
+// }
+// export const secret = async (key: SecretKey) => secrets[key];
+
+export const secret = async (key: SecretKey) =>
+  (await Bun.file((process.env.SECRETS_DIR || '/run/secrets/') + key).text()).trim();
