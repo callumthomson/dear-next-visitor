b1fe1ad61dc10f312f1d14dabd70f11fc5be2356
Callum Thomson <callum@callumthomson.co.uk>
2026-09-22 11:32:00 +0100

    Remove all Terraform configuration files for AWS infrastructure
    - Delete Terraform scripts managing ECS, ECR, ALB, CloudFront, Route 53, and SSM.
    - Remove `.terraform.lock.hcl` file and associated provider versioning.

--- infra/tf/ssm.tf
+++
@@ -1,9 +0,0 @@
-data "aws_ssm_parameter" "openai_api_key" {
-  name = "/manual/dear-next-visitor/OPENAI_API_KEY"
-}
-data "aws_ssm_parameter" "libsql_url" {
-  name = "/manual/dear-next-visitor/LIBSQL_DB_URL"
-}
-data "aws_ssm_parameter" "libsql_token" {
-  name = "/manual/dear-next-visitor/LIBSQL_DB_TOKEN"
-}
